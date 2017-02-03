719074f65776df56d75882612e9d8984325e3bb1
Developer <mjasso@itexico.net>
2017-02-03 10:56:13 -0600

    after cleanning fuckups

--- dbContainer/entrypoint.sh
+++ dbContainer/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "running ... $@"
-
-
 if [ "${1:0:1}" = '-' ]; then
 	set -- mongod "$@"
 fi
@@ -11,14 +8,7 @@ fi
 # allow the container to be started with `--user`
 if [ "$1" = 'mongod' -a "$(id -u)" = '0' ]; then
 	chown -R mongodb /data/configdb /data/db
-
-
-  gosu mongodb mongod --storageEngine=wiredTiger &
-	gosu mongodb mongo < /var/workdir/dbSetup.js
-  echo "Hello Hello!"
-
-
-	#exec gosu mongodb "$BASH_SOURCE" "$@"
+	exec gosu mongodb "$BASH_SOURCE" "$@"
 fi
 
 if [ "$1" = 'mongod' ]; then
