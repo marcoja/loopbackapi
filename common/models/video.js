4ac1b28d11ba904381d72ad253ff54239a9096f3
Marco Jasso <mjasso@itexico.net>
2017-02-06 21:12:58 -0600

    stage2- remote-method search is ready

--- common/models/video.js
+++ common/models/video.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Video) {
-
+  Video.search = function(cb) {
+    var response = 'Hello I am the remote method search!';
+    cb(null, response);
+  };
+  Video.remoteMethod('search', {
+    http: {path: '/search', verb: 'get'},
+    returns: {arg: 'search', type: 'string'},
+  });
 };
